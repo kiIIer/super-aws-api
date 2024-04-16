70f2ab886cc2649fe1d4039b49723d4a970c9358
kiIIer <39669501+kiIIer@users.noreply.github.com>
2024-04-16 09:16:31 +0300

    added a simple comment (#3)

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// handler is the function that will be called when the server receives a request
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi")
 }
