3d81ec0ab927494260a1f86ca39fab495afdefdb
kiIIer <39669501+kiIIer@users.noreply.github.com>
2024-04-16 16:00:26 +0300

    Ssl (#12)
    * added sertificate to the api
    
    * updated workflow to include certificate
    
    * excluded folder with private stuff

--- main.go
+++ main.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	defaultCertPath := "env/cert.pem"
+	defaultKeyPath := "env/key.pem"
+
+	certPath := os.Getenv("CERT_PATH")
+	if certPath == "" {
+		certPath = defaultCertPath
+	}
+
+	keyPath := os.Getenv("KEY_PATH")
+	if keyPath == "" {
+		keyPath = defaultKeyPath
+	}
+
+	log.Fatal(http.ListenAndServeTLS(":443", certPath, keyPath, nil))
 }
 
 // handler is the function that will be called when the server receives a request
