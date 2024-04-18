6b0db5723f209b57d4449e4175a195541134e380
killer <michael.molchanov.2004@gmail.com>
2024-04-18 13:30:31 +0200

    updated test file

--- main.go
+++ main.go
@@ -34,5 +34,5 @@ func main() {
 
 // handler is the function that will be called when the server receives a request
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I changed this")
+	fmt.Fprintf(w, "Lab defended")
 }
