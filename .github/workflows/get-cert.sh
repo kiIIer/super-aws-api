52cb01b38ae7572215c019eca2345cbf8afa1de8
killer <michael.molchanov.2004@gmail.com>
2024-04-18 08:35:20 +0200

    deploy to aws and cert script

--- .github/workflows/get-cert.sh
+++ .github/workflows/get-cert.sh
@@ -1,22 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 
 CONTAINER_NAME="super-aws-api"
 VOLUME_PATH="/etc/ssl"
+EMAIL="michael.molchanov.2004@gmail.com"
 
-docker stop $CONTAINER_NAME
+sudo docker stop $CONTAINER_NAME
+sudo docker rm $CONTAINER_NAME
 
-certbot renew --non-interactive --standalone
+if ! command -v certbot > /dev/null; then
+    sudo snap install core; sudo snap refresh core
+    sudo snap install --classic certbot
+    sudo ln -sf /snap/bin/certbot /usr/bin/certbot
+fi
 
-mkdir -p $VOLUME_PATH
+if [ ! -d "/etc/letsencrypt/live/kiiier.top" ]; then
+    sudo certbot certonly --non-interactive --standalone --agree-tos --email $EMAIL -d kiiier.top
+else
+    sudo certbot renew --non-interactive --standalone --agree-tos --email $EMAIL
+fi
 
-cp /etc/letsencrypt/live/ec2-16-16-57-192.eu-north-1.compute.amazonaws.com/fullchain.pem $VOLUME_PATH/cert.pem
-cp /etc/letsencrypt/live/ec2-16-16-57-192.eu-north-1.compute.amazonaws.com/privkey.pem $VOLUME_PATH/key.pem
+if [ -f "/etc/letsencrypt/live/kiiier.top/fullchain.pem" ] && [ -f "/etc/letsencrypt/live/kiiier.top/privkey.pem" ]; then
+    sudo mkdir -p $VOLUME_PATH
+    sudo cp /etc/letsencrypt/live/kiiier.top/fullchain.pem $VOLUME_PATH/cert.pem
+    sudo cp /etc/letsencrypt/live/kiiier.top/privkey.pem $VOLUME_PATH/key.pem
+    sudo chmod 644 $VOLUME_PATH/cert.pem
+    sudo chmod 600 $VOLUME_PATH/key.pem
 
-chmod 644 $VOLUME_PATH/cert.pem
-chmod 600 $VOLUME_PATH/key.pem
-
-docker run -d --name $CONTAINER_NAME -p 443:443 \
-  -e CERT_PATH="$VOLUME_PATH/cert.pem" \
-  -e KEY_PATH="$VOLUME_PATH/key.pem" \
-  -v $VOLUME_PATH:$VOLUME_PATH:ro \
-  kiiier/super-aws-api:latest
+    sudo docker run -d --name $CONTAINER_NAME -p 443:443 \
+      -e CERT_PATH="$VOLUME_PATH/cert.pem" \
+      -e KEY_PATH="$VOLUME_PATH/key.pem" \
+      -v $VOLUME_PATH:$VOLUME_PATH:ro \
+      kiiier/super-aws-api:latest
+else
+    echo "Certificate or key file not found, aborting."
+fi
