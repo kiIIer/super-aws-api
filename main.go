a8778829f34a7990278cef0e7e0d454f7e57d07d
killer <michael.molchanov.2004@gmail.com>
2024-04-18 08:30:13 +0200

    how serving http as well

--- main.go
+++ main.go
@@ -10,20 +10,26 @@ import (
 func main() {
 	http.HandleFunc("/", handler)
 
-	defaultCertPath := "env/cert.pem"
-	defaultKeyPath := "env/key.pem"
+	// Start the HTTPS server in a new goroutine
+	go func() {
+		defaultCertPath := "env/cert.pem"
+		defaultKeyPath := "env/key.pem"
 
-	certPath := os.Getenv("CERT_PATH")
-	if certPath == "" {
-		certPath = defaultCertPath
-	}
+		certPath := os.Getenv("CERT_PATH")
+		if certPath == "" {
+			certPath = defaultCertPath
+		}
 
-	keyPath := os.Getenv("KEY_PATH")
-	if keyPath == "" {
-		keyPath = defaultKeyPath
-	}
+		keyPath := os.Getenv("KEY_PATH")
+		if keyPath == "" {
+			keyPath = defaultKeyPath
+		}
 
-	log.Fatal(http.ListenAndServeTLS(":443", certPath, keyPath, nil))
+		log.Fatal(http.ListenAndServeTLS(":443", certPath, keyPath, nil))
+	}()
+
+	// Start the HTTP server
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 // handler is the function that will be called when the server receives a request
